2ba8335c536bd6c00378cb9c06ad6fef1cc24490
vetobit <vetobit@yandex.ru>
2016-04-29 13:09:38 +0300

    Написал коментарии в код

--- js/main.js
+++ js/main.js
@@ -1,48 +1,48 @@
-document.addEventListener("DOMContentLoaded",function(){
-  obj.init(); 
+document.addEventListener("DOMContentLoaded",function(){                                          // при загрузки страницы
+  obj.init();                                                                                     // выполнить функцию инициализации
 });
 
-var obj={
-  user:{
-    name:"admin",
-    password:"vetobit"
+var obj={                                                                                         // Объявляем главный объект программы
+  user:{                                                                                          // Данные о пользователе, который будет собирать модуль программы
+    name:"admin",                                                                                 // Имя пользователя
+    password:"vetobit"                                                                            // Пароль пользователя
   },
-  init:function(){
-    this.authorizationStart(this.user.name,this.user.password,"json/users.json",function(){
-      if(obj.authorization){
-        obj.getDataFromJson("json/data.json");
+  init:function(){                                                                                // Функция инициализации
+    this.authorizationStart(this.user.name,this.user.password,"json/users.json",function(){       // Запуск функции авторизации с параметрами (имя, пароль, путь до файла с пользователями, функция после получения данных об авторизации)
+      if(obj.authorization){                                                                      // Если авторизация успешна
+        obj.getDataFromJson("json/data.json");                                                    // Выполняем функцию загрузки всех товаров по пути к файлу товаров
       }
     });
   },
-  getDataFromJson:function (url){
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-    xhr.onreadystatechange = function(){
-      if(xhr.readyState==4 && xhr.status==200){
-        obj.data=JSON.parse(xhr.responseText);
+  getDataFromJson:function (url){                                                                 // Функция получения товаров из файла
+    var xhr = new XMLHttpRequest();                                                               // Объявляем новый объект соединения
+    xhr.open("GET", url, true);                                                                   // Задаём условия соединения (метод GET, путь до файла с товарами, ассихронная загрузка)
+    xhr.onreadystatechange = function(){                                                          // Объявляем функцию после завершения подготовки объекта соединения
+      if(xhr.readyState==4 && xhr.status==200){                                                   // Если объект соединения завершил подготовку и статус сервера вернул положительный результат
+        obj.data=JSON.parse(xhr.responseText);                                                    // То записываем в объект программы новый массив для хранения товаров и записываем в него данные из файла
       }
     };
-    xhr.send(null);
+    xhr.send(null);                                                                               // Отправляем пустой запрос на сервер для получения ответа
   },
-  authorizationStart:function(userName,password,url,func){
-    var xhr = new XMLHttpRequest();
+  authorizationStart:function(userName,password,url,func){                                        // Функция начала авторизации. Принимает (Имя пользователя, пароль, путь до файла с пользователями, функция инициализации)
+    var xhr = new XMLHttpRequest();                                                               // Объявляем новый объект соединения
 
-    obj.authorization = false;
-    xhr.open("GET", url, true);
-    xhr.onreadystatechange = function(){
-      if(xhr.readyState==4 && xhr.status==200){
-        JSON.parse(xhr.responseText).map(function(responseUser){
-          if(responseUser.userName==userName && responseUser.password==password){
-            obj.authorization = true;
-            obj.user.privileges = responseUser.privileges;
+    obj.authorization = false;                                                                    // Записываем в основной объект новую переменную, которая будет хранить ответ авторизации и записываем отрицательный результат
+    xhr.open("GET", url, true);                                                                   // Задаём условия соединения (метод GET, путь до файла с пользователями, ассихронная загрузка)
+    xhr.onreadystatechange = function(){                                                          // Объявляем функцию после завершения подготовки объекта соединения
+      if(xhr.readyState==4 && xhr.status==200){                                                   // Если объект соединения завершил подготовку и статус сервера вернул положительный результат
+        JSON.parse(xhr.responseText).map(function(responseUser){                                  // Преобразуем полученные данные в объект и, так как это массив объектов, то перебираем его объекты. Для каждого такого объекта выполняется функция в который он передаётся
+          if(responseUser.userName==userName && responseUser.password==password){                 // Если имя и пароль в объекте совпадают с именем и паролем пользователя
+            obj.authorization = true;                                                             // Записываем в свойство авторизации основного объекта программы положительный результат
+            obj.user.privileges = responseUser.privileges;                                        // Создаём новое свойство в пользователе основного объекта программы, которое будет хранить тип пользователя и записываем значение из объекта
           }
         });
-        obj.authorizationEnd(func);
+        obj.authorizationEnd(func);                                                               // Вызываем функцию конца авторизации и передаём в неё функцию инициализации
       }
     };
-    xhr.send(null);
+    xhr.send(null);                                                                               // Отправляем пустой запрос на сервер для получения ответа
   },
-  authorizationEnd:function(func){
-    func();
+  authorizationEnd:function(func){                                                                // Функция конца авторизации, которая принимает функцию инициализации
+    func();                                                                                       // Вызываем функцию инициализации
   }
 };
